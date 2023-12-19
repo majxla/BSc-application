7f9adee1e8cbb5e6541773cace15f2d9181862fe
Maja Potęga <83873656+majxla@users.noreply.github.com>
2023-12-19 22:19:34 +0100

    Update settings.py

--- tripsapp/settings.py
+++ tripsapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3lyd^(*h73sgs0k9=40l*)z!+gy23pzfjs7w3kdq_#y-dlu=lr'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
